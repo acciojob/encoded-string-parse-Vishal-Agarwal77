6cb7f6846cf36dfb7b596eb6d17f4bceac664f71
Vishal-Agarwal77 <108511250+Vishal-Agarwal77@users.noreply.github.com>
2023-11-20 02:07:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
 const parseCode = (str) => {
   // your code here
+	let f_Name="";
+	let l_name="";
+	let id="";
+	let i=0;
+	while(str.charAt(i)!=="0"){
+		f_Name+=str.charAt(i);
+		i++;
+	}
+	while(str.charAt(i)==="0"){
+		i++;
+	}
+	while(str.charAt(i)!=="0"){
+		l_name+=str.charAt(i);
+		i++;
+	}
+	while(str.charAt(i)==="0"){
+		i++;
+	}
+	while(i<str.length && str.charAt(i)!=="0"){
+		id+=str.charAt(i);
+		i++;
+	}
+	return {
+		firstName: f_Name,
+		lastName: l_name,
+		id: id
+	}
 };
 
-// Do not change the code below
+// Do not change the code belo
 const str = prompt("Enter str: ");
 alert(JSON.stringify(parseCode(str)));
